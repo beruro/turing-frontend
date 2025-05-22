beffca90407efb55b941ef46de0bcb6bf09e423b
beruro <105903607+beruro@users.noreply.github.com>
2025-05-22 08:20:46 +0000

    Deploying to gh-pages from @ beruro/turing-frontend@71f2e704be7c63d2987a17fb2e708e3045e16149 🚀

---
+++ meta__water-mark.0807adcd.async.js
@@ -0,0 +1,91 @@
+"use strict";(self.webpackChunkturing_react_native_ui=self.webpackChunkturing_react_native_ui||[]).push([[2137],{16102:function(u,a,e){var t;e.r(a),e.d(a,{demos:function(){return f}});var m=e(15009),d=e.n(m),x=e(99289),p=e.n(x),r=e(67294),s=e(77884),v=e(1078),f={"src-water-mark-demo-base":{component:r.memo(r.lazy(function(){return e.e(2433).then(e.bind(e,11731))})),asset:{type:"BLOCK",id:"src-water-mark-demo-base",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:e(29439).Z},react:{type:"NPM",value:"18.3.1"},"turing-react-native-ui":{type:"NPM",value:"0.1.0"}},entry:"index.tsx",description:"\u7ED9\u6307\u5B9A\u5185\u5BB9\u6DFB\u52A0\u6587\u5B57\u3002",title:"\u57FA\u7840\u7528\u6CD5"},context:{react:t||(t=e.t(r,2)),"react-native":s,"turing-react-native-ui":v},renderOpts:{compile:function(){var l=p()(d()().mark(function I(){var o,i=arguments;return d()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,e.e(4019).then(e.bind(e,4019));case 2:return n.abrupt("return",(o=n.sent).default.apply(o,i));case 3:case"end":return n.stop()}},I)}));function c(){return l.apply(this,arguments)}return c}()}},"src-water-mark-demo-foreground":{component:r.memo(r.lazy(function(){return e.e(2433).then(e.bind(e,50349))})),asset:{type:"BLOCK",id:"src-water-mark-demo-foreground",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:e(59116).Z},react:{type:"NPM",value:"18.3.1"},"turing-react-native-ui":{type:"NPM",value:"0.1.0"}},entry:"index.tsx",description:"\u524D\u7F6E\u6C34\u5370\uFF0C\u4E0D\u53D7\u5185\u5BB9\u906E\u6321\u3002",title:"\u524D\u7F6E\u6C34\u5370"},context:{react:t||(t=e.t(r,2)),"react-native":s,"turing-react-native-ui":v},renderOpts:{compile:function(){var l=p()(d()().mark(function I(){var o,i=arguments;return d()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,e.e(4019).then(e.bind(e,4019));case 2:return n.abrupt("return",(o=n.sent).default.apply(o,i));case 3:case"end":return n.stop()}},I)}));function c(){return l.apply(this,arguments)}return c}()}}}},5774:function(u,a,e){e.r(a),e.d(a,{texts:function(){return t}});const t=[{value:"0.3.0+",paraId:0},{value:"\u9875\u9762\u4E0A\u6DFB\u52A0\u7279\u5B9A\u7684\u6587\u5B57\u3002",paraId:1,tocIndex:0},{value:"\u5C5E\u6027\u540D",paraId:2,tocIndex:4},{value:"\u63CF\u8FF0",paraId:2,tocIndex:4},{value:"\u7C7B\u578B",paraId:2,tocIndex:4},{value:"\u9ED8\u8BA4\u503C",paraId:2,tocIndex:4},{value:"\u7248\u672C",paraId:2,tocIndex:4},{value:"text",paraId:2,tocIndex:4},{value:"\u6587\u5B57\u5185\u5BB9",paraId:2,tocIndex:4},{value:"string",paraId:2,tocIndex:4},{value:"-",paraId:2,tocIndex:4},{value:"-",paraId:2,tocIndex:4},{value:"color",paraId:2,tocIndex:4},{value:"\u6587\u5B57\u989C\u8272",paraId:2,tocIndex:4},{value:"ColorValue",paraId:2,tocIndex:4},{value:"water_mark_text_color",paraId:2,tocIndex:4},{value:"-",paraId:2,tocIndex:4},{value:"fontSize",paraId:2,tocIndex:4},{value:"\u5B57\u4F53\u5927\u5C0F",paraId:2,tocIndex:4},{value:"number",paraId:2,tocIndex:4},{value:"water_mark_text_font_size",paraId:2,tocIndex:4},{value:"-",paraId:2,tocIndex:4},{value:"opacity",paraId:2,tocIndex:4},{value:"\u6587\u5B57\u900F\u660E\u5EA6",paraId:2,tocIndex:4},{value:"number",paraId:2,tocIndex:4},{value:"water_mark_text_opacity",paraId:2,tocIndex:4},{value:"-",paraId:2,tocIndex:4},{value:"textWidth",paraId:2,tocIndex:4},{value:"\u6587\u5B57\u5BBD",paraId:2,tocIndex:4},{value:"number",paraId:2,tocIndex:4},{value:"64",paraId:2,tocIndex:4},{value:"-",paraId:2,tocIndex:4},{value:"textHeight",paraId:2,tocIndex:4},{value:"\u6587\u5B57\u9AD8/\u884C\u9AD8",paraId:2,tocIndex:4},{value:"number",paraId:2,tocIndex:4},{value:"64",paraId:2,tocIndex:4},{value:"-",paraId:2,tocIndex:4},{value:"rotate",paraId:2,tocIndex:4},{value:"\u6C34\u5370\u7ED8\u5236\u65F6\uFF0C\u65CB\u8F6C\u7684\u89D2\u5EA6\uFF0C\u5355\u4F4D \xB0",paraId:2,tocIndex:4},{value:"number",paraId:2,tocIndex:4},{value:"-45",paraId:2,tocIndex:4},{value:"-",paraId:2,tocIndex:4},{value:"foreground",paraId:2,tocIndex:4},{value:"\u5426\u9700\u8981\u524D\u7F6E\u6C34\u5370",paraId:2,tocIndex:4},{value:"boolean",paraId:2,tocIndex:4},{value:"false",paraId:2,tocIndex:4},{value:"-",paraId:2,tocIndex:4},{value:"\u540D\u79F0",paraId:3,tocIndex:5},{value:"\u9ED8\u8BA4\u503C",paraId:3,tocIndex:5},{value:"\u63CF\u8FF0",paraId:3,tocIndex:5},{value:"water_mark_text_font_size",paraId:3,tocIndex:5},{value:"TOKENS.font_size_2",paraId:3,tocIndex:5},{value:"-",paraId:3,tocIndex:5},{value:"water_mark_text_color",paraId:3,tocIndex:5},{value:"TOKENS.gray_6",paraId:3,tocIndex:5},{value:"-",paraId:3,tocIndex:5},{value:"water_mark_text_opacity",paraId:3,tocIndex:5},{value:"TOKENS.opacity_10",paraId:3,tocIndex:5},{value:"-",paraId:3,tocIndex:5}]},29439:function(u,a){a.Z=`import React from 'react'
+import { ScrollView, View } from 'react-native'
+
+import { WaterMark, Blank, Space } from 'turing-react-native-ui'
+
+const UploaderBase: React.FC = () => {
+  return (
+    <WaterMark text="\u4EC5\u7528\u4E8E\u6848\u4F8B\u5C55\u793A" textWidth={90}>
+      <ScrollView>
+        <Blank>
+          <Space>
+            <View
+              style={{
+                height: 300,
+                width: 200,
+                alignSelf: 'center',
+                backgroundColor: '#f30',
+              }}
+            />
+
+            <View
+              style={{
+                height: 300,
+                width: 200,
+                alignSelf: 'center',
+                backgroundColor: '#098',
+              }}
+            />
+
+            <View
+              style={{
+                height: 300,
+                width: 200,
+                alignSelf: 'center',
+                backgroundColor: '#678',
+              }}
+            />
+          </Space>
+        </Blank>
+      </ScrollView>
+    </WaterMark>
+  )
+}
+
+export default UploaderBase
+`},59116:function(u,a){a.Z=`import React from 'react'
+import { ScrollView, View } from 'react-native'
+
+import { WaterMark, Blank, Space } from 'turing-react-native-ui'
+
+const UploaderBase: React.FC = () => {
+  return (
+    <WaterMark
+      text="\u4EC5\u7528\u4E8E\u6848\u4F8B\u5C55\u793A"
+      foreground
+      color="#fff"
+      opacity={0.2}
+      fontSize={18}
+      textWidth={130}>
+      <ScrollView>
+        <Blank>
+          <Space>
+            <View
+              style={{
+                height: 300,
+                backgroundColor: '#f30',
+              }}
+            />
+
+            <View
+              style={{
+                height: 300,
+                backgroundColor: '#098',
+              }}
+            />
+
+            <View
+              style={{
+                height: 300,
+                backgroundColor: '#678',
+              }}
+            />
+          </Space>
+        </Blank>
+      </ScrollView>
+    </WaterMark>
+  )
+}
+
+export default UploaderBase
+`}}]);
